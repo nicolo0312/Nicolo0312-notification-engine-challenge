fa3b4f539ad8a6bd5da222fe27d56c6040858dcb
nicolo0312 <nicomoran_2@hotmail.com>
2026-04-09 11:01:32 -0300

    feat: add initial DocumentsExpiring implementation

--- src/handler.ts
+++ src/handler.ts
@@ -1,16 +1,23 @@
 import { parseEvent, DomainEvent } from './events';
+import { FakeMachineRepository } from './repositories/machine.repository';
 import { FakeUserRepository } from './repositories/user.repository';
+
+const PROJECT_MANAGER = 'project_manager';
+const COMPANY_ADMIN = 'company_admin';
+const SITE_SUPERVISOR = 'site_supervisor';
 
 
 export function handler(rawEvent: unknown): unknown {
   try {
     const userRepo = new FakeUserRepository();
+    const machineRepo = new FakeMachineRepository();
     const event: DomainEvent = parseEvent(rawEvent);
+    let notifications: any[] = []
   
     switch(event['detail-type']){
       case 'MachineMoved':
         const {to_project_id} = event.detail;
-        const supervisors = userRepo.getUsersByRoleInProject(to_project_id, 'site_supervisor')
+        const supervisors = userRepo.getUsersByRoleInProject(to_project_id, SITE_SUPERVISOR)
         return supervisors.map((user) => ({
           recipient_id: user.id,
           payload: event.detail,
@@ -19,12 +26,10 @@ export function handler(rawEvent: unknown): unknown {
       case 'OOSReportBatchCreated':
 
       const projectsIds = new Set(event.detail.reports.map((r) => r.project_id))
-        const notifications: any[] = [];
-
         for (const projectId of projectsIds) {
           const pms = userRepo.getUsersByRoleInProject(
             projectId,
-            'project_manager'
+            PROJECT_MANAGER
           );
           for (const pm of pms){
             notifications.push({
@@ -36,7 +41,19 @@ export function handler(rawEvent: unknown): unknown {
         return notifications;
   
       case 'DocumentsExpiring':
-        return [];
+        for (const doc of event.detail.documents) {
+          const machine = machineRepo.getById(doc.machine_id);
+          if(!machine) continue;
+          const admins = userRepo.getUsersByRoleInCompany(machine.company_id, COMPANY_ADMIN)
+
+          for (const admin of admins) {
+            notifications.push({
+              recipient_id: admin.id,
+              payload: { documents: [doc]}
+            })
+          }
+        }
+        return notifications;
   
       default:
         throw new Error('Unhandled event type')
