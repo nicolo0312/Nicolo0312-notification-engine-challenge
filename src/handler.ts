a55a23bcde75fdaa9d270fc92f571264364efbc3
nicolo0312 <nicomoran_2@hotmail.com>
2026-04-08 21:47:12 -0300

    feat: implement MachineMoved notification rule

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,20 @@
 import { parseEvent, DomainEvent } from './events';
+import { FakeUserRepository } from './repositories/user.repository';
 
 
 export function handler(rawEvent: unknown): unknown {
   try {
+    const userRepo = new FakeUserRepository();
     const event: DomainEvent = parseEvent(rawEvent);
   
     switch(event['detail-type']){
       case 'MachineMoved':
-        return [];
+        const {to_project_id} = event.detail;
+        const supervisors = userRepo.getUsersByRoleInProject(to_project_id, 'site_supervisor')
+        return supervisors.map((user) => ({
+          recipient_id: user.id,
+          payload: event.detail,
+        }));
       
       case 'OOSReportBatchCreated':
         return [];
